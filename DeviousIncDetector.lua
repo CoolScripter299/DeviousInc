bf4985b66f87b129d50ddfa24b5baa1e9d9e9821
CoolScripter299 <82603186+CoolScripter299@users.noreply.github.com>
2022-11-13 15:04:48 -0500

    Add files via upload

---
+++ DeviousIncDetector.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 10758111998 or 10767870749 then
+	loadstring(game:HttpGet("https://github.com/CoolScripter299/DeviousInc/blob/main/DeviousInc%20V2.11.lua"))()
+else
+	game.Players.LocalPlayer:Kick("This isn't Booga Booga!")
+end
