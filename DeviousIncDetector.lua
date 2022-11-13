72f139d0865f2023bf56bb5510e5b4b516592d3a
CoolScripter299 <82603186+CoolScripter299@users.noreply.github.com>
2022-11-13 15:15:44 -0500

    Update DeviousIncDetector.lua

--- DeviousIncDetector.lua
+++ DeviousIncDetector.lua
@@ -1,5 +1,7 @@
-if game.PlaceId == 10758111998 or 10767870749 then
+if game.PlaceId == 10758111998 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/CoolScripter299/DeviousInc/main/DeviousInc%20V2.11.lua"))()
-elseif
+elseif game.PlaceId == 10767870749 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CoolScripter299/DeviousInc/main/DeviousInc%20V2.11.lua"))()
+else
 	game.Players.LocalPlayer:Kick("This isn't Booga Booga!")
 end
