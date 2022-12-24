678f513f892fd31ca86e2410ccac7e4506bcb743
CoolScripter299 <82603186+CoolScripter299@users.noreply.github.com>
2022-12-23 22:21:20 -0500

    Update MassDeviousness.lua

--- MassDeviousness.lua
+++ MassDeviousness.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 10758111998 then
+if game.PlaceId == 11729688377 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/CoolScripter299/DeviousInc/main/DeviousInc%20V2.12.lua"))()
 elseif game.PlaceId == 10767870749 then
     	loadstring(game:HttpGet("https://raw.githubusercontent.com/CoolScripter299/DeviousInc/main/DeviousInc%20V2.12.lua"))()
