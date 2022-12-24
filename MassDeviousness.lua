c06a454694ac9b095fd953c5b399a02dfabd4357
CoolScripter299 <82603186+CoolScripter299@users.noreply.github.com>
2022-12-24 00:58:08 -0500

    Update MassDeviousness.lua

--- MassDeviousness.lua
+++ MassDeviousness.lua
@@ -1,6 +1,6 @@
-if game.PlaceId == 11729688333 then
+	if game.PlaceId == 11729688377 then -- Overworld
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/CoolScripter299/DeviousInc/main/DeviousInc%20V2.2.lua"))()
-elseif game.PlaceId == 10767870749 then
+elseif game.PlaceId == 10767870749 then -- void
     	loadstring(game:HttpGet("https://raw.githubusercontent.com/CoolScripter299/DeviousInc/main/DeviousInc%20V2.2.lua"))()
 else
 	game.Players.LocalPlayer:Kick("This isn't Booga Booga!")
