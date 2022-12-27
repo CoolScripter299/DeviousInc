684806116d50ef5abfbf49154ed2947837241317
CoolScripter299 <82603186+CoolScripter299@users.noreply.github.com>
2022-12-27 02:56:34 -0500

    Update MassDeviousness.lua

--- MassDeviousness.lua
+++ MassDeviousness.lua
@@ -1,7 +1,7 @@
 	if game.PlaceId == 11729688377 then -- Overworld
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/CoolScripter299/DeviousInc/main/DeviousInc%20V2.2.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/CoolScripter299/DeviousInc/main/DeviousInc%20V3.lua"))()
 elseif game.PlaceId == 11879754496 then -- void
-    	loadstring(game:HttpGet("https://raw.githubusercontent.com/CoolScripter299/DeviousInc/main/DeviousInc%20V2.2.lua"))()
+    	loadstring(game:HttpGet("https://raw.githubusercontent.com/CoolScripter299/DeviousInc/main/DeviousInc%20V3.lua"))()
 else
 	game.Players.LocalPlayer:Kick("Fucking Faggot ass loser, this isnt booga booga, get ur ass on that game bitch(CALAMITY)")
 end
